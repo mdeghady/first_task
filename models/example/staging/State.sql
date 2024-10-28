2734c478139d9b6c928b7528cfb17abac2988aaa
mdeghady <mostafadeghady22@gmail.com>
2024-10-28 20:30:57 +0300

    add State model sql query

---
+++ models/example/staging/State.sql
@@ -0,0 +1,12 @@
+WITH all_data_states AS(
+    SELECT state
+    FROM {{ source('main_Sources' , 'dbusa_property') }}
+    UNION
+    SELECT state
+    FROM {{ source('main_Sources' , 'reonomy_property') }}
+)
+
+--- define city table with generated id
+SELECT state , ROW_NUMBER() OVER (ORDER BY state) AS id
+FROM all_data_states
+WHERE state IS NOT NULL
